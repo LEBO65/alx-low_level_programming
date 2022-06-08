44c22b3cd36a87da1111d26361c143d76e9ce7ea
LEBO65 <merelenzy45@gmail.com>
2022-06-08 05:36:16 -0700

    4-task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _isalpha - checks for alphabetical letters
+*test_isalpha - checks for alphabetical letters
 * @c: a charter to be checke on
 * Return: returns 0 or 1 depending on condition
 */
