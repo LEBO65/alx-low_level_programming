404f7b772317439c122500d2ea31ae11fc5961c1
LEBO65 <merelenzy45@gmail.com>
2022-06-14 13:08:42 -0700

    function that prints a new string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts -> a function to put
+ * @str: a place to _puts function
+ */
+void _puts(char *str);
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+	_puthar('\n');
+}
