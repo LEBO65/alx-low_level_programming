94e285c422d070827e32e5c43305803af2a84833
LEBO65 <merelenzy45@gmail.com>
2022-06-14 14:12:48 -0700

    leve 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -> printing reverse strings
+ * @s: print string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = strlen(s);
+	for (i = n - 1; 1 >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
