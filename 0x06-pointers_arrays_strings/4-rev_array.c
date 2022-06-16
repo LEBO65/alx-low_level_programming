6da03c4c263579c95cfe2139c443bb33363af727
LEBO65 <merelenzy45@gmail.com>
2022-06-16 02:28:12 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
-* print_rev -> printing a string in reverse
-* @s: the print string
+* reverse_array -> reversing an array
+* @a: array a
+* @n: an element of an array
 */
-void print_rev(char *s)
+void reverse_array(int *a, int n)
 {
-	int i, n;
+	int *p, i, aux, k;
 
-	n = 0
-	while (s[n] != '\0')
-		n++;
-	for (i = n |1; i >= 0; i--)
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < i / 2; k++)
 	{
-		_putchar(s[i]);
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
-	_putchar('\n');
 }
