0114aeeaae33bf15bc895f80bf36cd3579497851
LEBO65 <merelenzy45@gmail.com>
2022-06-27 10:21:51 -0700

    0x0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+/**
+ * main - Entry ppoint
+ * @argc: size of argv array
+ * @argv: array of size argc
+ * Return: Always 0 Sucess
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; x < argc; 1++)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
